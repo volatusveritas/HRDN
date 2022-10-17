fde1d22a9de7cf03316a2837f1a0a78447636c25
sogekng <gutsantos3@gmail.com>
2022-10-17 16:06:57 -0300

    add controllers: create/delte/edit/update *actions -> obvious

--- hrdn/api/controllers/CustomersController.js
+++ hrdn/api/controllers/CustomersController.js
@@ -1,4 +1,9 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable no-unused-vars */
+/* eslint-disable curly */
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable linebreak-style */
 /* eslint-disable indent */
 /**
  * CustomersController
@@ -8,19 +13,73 @@
  */
 
 module.exports = {
-    'customers': function(req, res) {
-        res.view('customer/customers');
-        res.redirect('/');
-    },
 
     'new': function(req, res) {
         res.view('customer/new');
         res.redirect('/');
     },
 
-    'delete': function(req, res){
-        res.view('customer/delete');
-        res.redirect('/');
+    list:function(req, res) {
+        Customers.find({}).exec(function(err, customers){
+            if(err){
+                res.send(500, {error: 'Não deu dale'});
+            }
+            res.view('customer/list', {customers:customers});
+        });
+    },
+
+    add:function(req, res){
+        res.view('customer/add');
+    },
+
+    create:function(req, res) {
+        var name = req.body.name;
+        var job = req.body.job;
+        var hiring = req.body.hiring;
+
+        Customers.create({name:name, job:job, hiring:hiring}).exec(function(err){
+            if(err){
+                res.send(500, {error: 'Nada dale'});
+            }
+
+            res.redirect('/customers/list');
+        });
+    },
+
+    delete: function(req, res){
+        Customers.destroy({id:req.params.id}).exec(function(err){
+            if(err){
+                res.send(500, {error: 'Nada dale'});
+            }
+
+            res.redirect('/customers/list');
+        });
+
+        return false;
+    },
+    edit: function(req, res){
+        Customers.findOne({id:req.params.id}).exec(function(err, customer){
+            if(err){
+                res.send(500, {error: 'Nada dale'});
+            }
+
+            res.view('customer/edit', {customer:customer});
+        });
+    },
+    update: function(req, res){
+        var name = req.body.name;
+        var job = req.body.job;
+        var hiring = req.body.hiring;
+
+        Customers.update({id: req.params.id},{name:name, job:job, hiring:hiring}).exec(function(err){
+            if(err){
+                res.send(500, {error: 'Nada dale'});
+            }
+
+            res.redirect('/customers/list');
+        });
+
+        return false;
     }
 };
 
